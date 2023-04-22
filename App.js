cdb5f1f37a785f093ddc4e668eebca17fa2cddbd
Rafael Nicolau <rafael.nicolau@sedo.com>
2023-04-23 00:49:05 +0200

    Add functionality in primitive state

--- App.js
+++ App.js
@@ -1,12 +1,90 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setNewTodo(enteredText);
+  };
+  const addGoalHandler = () => {
+    setTodoList((currentTodoList) => [
+      ...currentTodoList,
+      { text: newTodo, id: Math.random().toString() },
+    ]);
+  };
+
   return (
-    <View>
-      <View></View>
-      <View></View>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your ToDo"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add ToDo" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={todoList}
+          keyExtractor={(item) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.todoItem}>
+                <Text style={styles.todoText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  todoItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  todoText: {
+    color: "white",
+  },
+});
